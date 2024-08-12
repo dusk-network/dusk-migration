0ad6f93ff38e052fe14f15cb05bd533e949741ca
Hein Dauven <heindauven@gmail.com>
2024-08-12 09:57:47 +0200

    Add compiler configs and node scripts for hardhat

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -2,5 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      { version: "0.8.24" },
+      { version: "0.4.25" },
+    ]
+  },
+  overrides: {
+    "contracts/DUSKMigration.sol": {
+      version: "0.8.24",
+    },
+    "contracts/ERC20Mock.sol": {
+      version: "0.4.25",
+    }
+  }
 };
