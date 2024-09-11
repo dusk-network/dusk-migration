a77c04600b3ed7d69d52b841b88c6534f8597654
Hein Dauven <heindauven@gmail.com>
2024-09-12 01:36:41 +0200

    Adjust tests to check for proper conversion

--- contract/test/DUSKMigration.js
+++ contract/test/DUSKMigration.js
@@ -30,7 +30,11 @@ describe("Migration", function () {
         deployMigrationFixture
       );
 
-      const amountToMigrate = ethers.parseEther("100");
+      // We set the unit to 0 to work with the smallest value, test the DUSK to LUX conversion
+      // and see effect of the rounding down
+      const amountToMigrate = ethers.parseUnits("1234567890", 0);
+      const amountMigrated = ethers.parseUnits("1000000000", 0);
+      const luxToSend = ethers.parseUnits("1", 0);
       const phoenixAddress = "4ZH3oyfTuMHyWD1Rp4e7QKp5yK6wLrWvxHneufAiYBAjvereFvfjtDvTbBcZN5ZCsaoMo49s1LKPTwGpowik6QJG"
 
       // addr1 approves the migration contract
@@ -39,26 +43,26 @@ describe("Migration", function () {
       // Perform migration
       await expect(duskMigration.connect(addr1).migrate(amountToMigrate, phoenixAddress))
         .to.emit(duskMigration, "Migration")
-        .withArgs(addr1.address, amountToMigrate, phoenixAddress);
+        .withArgs(addr1.address, luxToSend, phoenixAddress); // Check that the event emits the correct value in LUX
 
-      // Check if the migration contract's balance increased
-      expect(await duskToken.balanceOf(duskMigration.target)).to.equal(amountToMigrate);
+      // Check if the migration contract's balance increased by the rounded down amount
+      expect(await duskToken.balanceOf(duskMigration.target)).to.equal(amountMigrated);
     });
 
-    it("Should revert if the user tries to migrate 0 tokens", async function () {
+    it("Should revert if the user tries to migrate less than 1 LUX", async function () {
       const { duskToken, duskMigration, addr2 } = await loadFixture(
         deployMigrationFixture
       );
 
-      const amountToMigrate = ethers.parseEther("0");
+      const amountToMigrate = ethers.parseUnits("123", 0); // Less than 1 LUX, this is 123 DUSK wei
       const phoenixAddress = "4ZH3oyfTuMHyWD1Rp4e7QKp5yK6wLrWvxHneufAiYBAjvereFvfjtDvTbBcZN5ZCsaoMo49s1LKPTwGpowik6QJG"
 
       // addr2 approves the migration contract
       await duskToken.connect(addr2).approve(duskMigration.target, amountToMigrate);
 
-      // Attempt to migrate 0 tokens, expect a revert
+      // Attempt to migrate less than 1 LUX, expect a revert
       await expect(duskMigration.connect(addr2).migrate(amountToMigrate, phoenixAddress))
-        .to.be.revertedWith("Amount must be greater than zero");
+        .to.be.revertedWith("Amount must be at least 1 LUX");
     });
 
     it("Should revert if the user has not approved enough tokens", async function () {
@@ -66,10 +70,10 @@ describe("Migration", function () {
         deployMigrationFixture
       );
 
-      const amountToMigrate = ethers.parseEther("100");
+      const amountToMigrate = ethers.parseEther("100"); // Try to migrate 100 DUSK
       const phoenixAddress = "4ZH3oyfTuMHyWD1Rp4e7QKp5yK6wLrWvxHneufAiYBAjvereFvfjtDvTbBcZN5ZCsaoMo49s1LKPTwGpowik6QJG"
 
-      // addr1 approves less tokens than needed
+      // addr1 approves less tokens than needed, only 50 DUSK
       await duskToken.connect(addr1).approve(duskMigration.target, ethers.parseEther("50"));
 
       // Attempt to migrate more tokens than approved, expect a revert
