b50e2731ccb6c16643d38fab3494e0ebf8267e8e
Hein Dauven <heindauven@gmail.com>
2024-09-12 11:43:23 +0200

    Improve gas usage, add more comments, rename min migrate amount to conversion factor

--- contract/contracts/DUSKMigration.sol
+++ contract/contracts/DUSKMigration.sol
@@ -11,9 +11,9 @@ interface IERC20 {
 }
 
 contract DUSKMigration {
-    IERC20 public duskToken;
-    // Minimum migratable amount: 10^9 wei, equivalent to 1 LUX on the native chain
-    uint256 constant MINIMUM_MIGRATION_AMOUNT = 10**9; 
+    IERC20 public immutable duskToken;
+    // Conversion factor between ERC20 DUSK (18 decimals) and native DUSK (9 decimals), where 10^9 DUSK wei is equivalent to 1 LUX
+    uint256 constant LUX_CONVERSION_FACTOR = 10**9; 
 
     // Event to log the migration for reissuing on Dusk mainnet. The amount being in LUX
     event Migration(address indexed from, uint256 amount, string targetAddress);
@@ -31,20 +31,23 @@ contract DUSKMigration {
      *      2. Effect: Transfers the specified amount of DUSK tokens from the sender to the contract.
      *      3. Interaction: Emits a `Migration` event, which is being relied upon for issuing native DUSK.
      * 
+     * @dev We assume that the targetAddress is a valid Moonlight key. The user will never explicitly type in the key, it will instead be provided by the UX.
+     * 
      * @param amount The amount of ERC20 DUSK tokens to migrate in DUSK wei. Must be at least 1 LUX (10^9 wei).
      * @param targetAddress The native DUSK mainnet Moonlight key where the equivalent native DUSK should be reissued.
      */
     function migrate(uint256 amount, string memory targetAddress) external {
-        require(amount >= MINIMUM_MIGRATION_AMOUNT, "Amount must be at least 1 LUX");
+        // The minimum migration amount has to be larger or equal to the conversion factor
+        require(amount >= LUX_CONVERSION_FACTOR, "Amount must be at least 1 LUX");
 
         // Round down the amount to the nearest multiple of 1 LUX
-        uint256 roundedAmount = amount / MINIMUM_MIGRATION_AMOUNT * MINIMUM_MIGRATION_AMOUNT;
+        uint256 roundedAmount = (amount / LUX_CONVERSION_FACTOR) * LUX_CONVERSION_FACTOR;
 
         // Transfer the specified amount of DUSK tokens to this contract
         duskToken.transferFrom(msg.sender, address(this), roundedAmount);
 
         // Adjust the amount to account for the difference in decimals between native DUSK (9 decimals) and ERC20/BEP20 DUSK (18 decimals)
-        uint256 nativeAmount = roundedAmount / 10**9;
+        uint256 nativeAmount = roundedAmount / LUX_CONVERSION_FACTOR;
 
         // Emit the migration event with the value in LUX
         emit Migration(msg.sender, nativeAmount, targetAddress);
